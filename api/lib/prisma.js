334578150889c8398fbde927a2195633590eeaa5
Harshthakur22 <143176083+Harshthakur22@users.noreply.github.com>
2025-10-18 22:45:14 +0530

    Update prisma.js

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 // Reuse a single Prisma client across hot reloads and serverless invocations
+// prisma as we can use sql or nosql any kind of database through it 
 const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
